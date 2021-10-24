207247d0f37a285dc054c1a8d143af65a4e8c661
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-10-24 22:15:18 +0100

    nhojpatrick-config - FileUtil#safeDirectoryName

--- core/src/main/java11/module-info.java
+++ core/src/main/java11/module-info.java
@@ -1,4 +1,5 @@
 module com.github.nhojpatrick.config.core {
     exports com.github.nhojpatrick.config.core;
+    requires org.apache.commons.lang3;
     requires org.slf4j;
 }
--- core/src/test/java/com/github/nhojpatrick/config/core/tests/FileUtilTest.java
+++ core/src/test/java/com/github/nhojpatrick/config/core/tests/FileUtilTest.java
@@ -1,5 +1,53 @@
 package com.github.nhojpatrick.config.core.tests;
 
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.github.nhojpatrick.config.core.FileUtil.safeDirectoryName;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FileUtilTest {
+
+    @ParameterizedTest
+    @MethodSource("safeDirectoryName_invalid_src")
+    void safeDirectoryName_invalid(final String input,
+                                   final String expectedMessage) {
+        final Executable testMethod = () -> safeDirectoryName(input);
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, testMethod);
+        assertAll("Checking Exception",
+                () -> assertThat(thrown.getMessage(), is(equalTo(expectedMessage))),
+                () -> assertThat(thrown.getCause(), is(nullValue()))
+        );
+    }
+
+    private static Stream<Arguments> safeDirectoryName_invalid_src() {
+        return Stream.of(
+                //
+                Arguments.of(null, "Invalid safeDirectoryName 'null'")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("safeDirectoryName_valid_src")
+    void safeDirectoryName_valid(final String input,
+                                 final String expected) {
+        assertThat(safeDirectoryName(input), is(equalTo(expected)));
+    }
+
+    private static Stream<Arguments> safeDirectoryName_valid_src() {
+        return Stream.of(
+                //
+                Arguments.of("qwerty", "qwerty")
+        );
+    }
 
 }
