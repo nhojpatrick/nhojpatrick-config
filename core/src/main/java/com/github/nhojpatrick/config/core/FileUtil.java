eb9e3b8540eb5b76f05155ae708fc72793590d57
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-10-24 16:17:42 +0100

    nhojpatrick-config - FileUtil

---
+++ core/src/main/java/com/github/nhojpatrick/config/core/FileUtil.java
@@ -0,0 +1,10 @@
+package com.github.nhojpatrick.config.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUtil.class);
+
+}
