bc44cd01c50f7f299ef6abe655b746479e2c0c90
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-30 08:06:25 +0100

    Break out FileUtil and safeDirectoryName tests

---
+++ core/src/main/java11/com/github/nhojpatrick/config/core/files/ModuleInfoHack.java
@@ -0,0 +1,4 @@
+package com.github.nhojpatrick.config.core.files;
+
+public class ModuleInfoHack {
+}
--- core/src/main/java11/module-info.java
+++ core/src/main/java11/module-info.java
@@ -1,5 +1,6 @@
 module com.github.nhojpatrick.config.core {
     exports com.github.nhojpatrick.config.core;
+    exports com.github.nhojpatrick.config.core.files;
     requires org.apache.commons.lang3;
     requires org.slf4j;
 }
