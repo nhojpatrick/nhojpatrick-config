36e3cafef60dd404ec69c571917948f98015d1ef
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-31 13:21:57 +0100

    Refactor EnvironmentConstantImpl to not use constants

--- core/src/main/java/com/github/nhojpatrick/config/core/internal/EnvironmentConstantImpl.java
+++ core/src/main/java/com/github/nhojpatrick/config/core/internal/EnvironmentConstantImpl.java
@@ -1,23 +1,27 @@
 package com.github.nhojpatrick.config.core.internal;
 
 import com.github.nhojpatrick.config.core.EnvironmentConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.util.Objects.requireNonNull;
 
 public class EnvironmentConstantImpl
         implements EnvironmentConstant {
 
-    public static final String ENV;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvironmentConstantImpl.class);
 
-    static {
-        ENV = initEnvSetup();
-    }
+    private final String envName;
 
-    public static final String initEnvSetup() {
-        return System.getProperty("env", "local_dev");
+    public EnvironmentConstantImpl() {
+        this.envName = System.getProperty("env", "local_dev");
+        requireNonNull(this.envName, "Required System Property 'env' missing.");
+        LOGGER.debug("EnvironmentConstantImpl for env='{}'", this.envName);
     }
 
     @Override
     public String getEnvName() {
-        return initEnvSetup();
+        return this.envName;
     }
 
 }
