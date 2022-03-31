77c09138dbc96673d42e4e719b8dcc3a23d75ec5
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-31 13:30:45 +0100

    Refactor EnvironmentConstant interface

--- core/src/test/java/com/github/nhojpatrick/config/core/tests/EnvironmentConstantImplTest.java
+++ core/src/test/java/com/github/nhojpatrick/config/core/tests/EnvironmentConstantImplTest.java
@@ -4,54 +4,40 @@ import com.github.nhojpatrick.config.core.EnvironmentConstantImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.junitpioneer.jupiter.ClearSystemProperty;
 import org.junitpioneer.jupiter.ReadsSystemProperty;
 import org.junitpioneer.jupiter.SetSystemProperty;
 
-import static com.github.nhojpatrick.config.core.EnvironmentConstantImpl.ENV;
-import static com.github.nhojpatrick.config.core.EnvironmentConstantImpl.initEnvSetup;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EnvironmentConstantImplTest {
 
     @Test
+    @ReadsSystemProperty
     public void constructor() {
-        final Executable testMethod = () -> new EnvironmentConstantImpl();
-        final AssertionError thrown = assertThrows(AssertionError.class, testMethod);
-        assertAll("Checking Exception",
-                () -> assertThat(thrown.getMessage(), is(equalTo("Static constants class - cannot be instantiated."))),
-                () -> assertThat(thrown.getCause(), is(nullValue()))
-        );
+        new EnvironmentConstantImpl();
     }
 
     @Nested
-    @DisplayName("EnvironmentConstantImpl.ENV")
-    class Env {
-
-        @Test
-        @ReadsSystemProperty
-        public void constant() {
-            assertThat(ENV, is(equalTo("local_dev")));
-        }
+    @DisplayName("EnvironmentConstantImpl.getEnvName")
+    class getEnvName {
 
         @Test
         @ReadsSystemProperty
         @ClearSystemProperty(key = "env")
         public void valueDefault() {
-            assertThat(initEnvSetup(), is(equalTo("local_dev")));
+            final EnvironmentConstantImpl cut = new EnvironmentConstantImpl();
+            assertThat(cut.getEnvName(), is(equalTo("local_dev")));
         }
 
         @Test
         @ReadsSystemProperty
         @SetSystemProperty(key = "env", value = "prod")
         public void valueCustom() {
-            assertThat(initEnvSetup(), is(equalTo("prod")));
+            final EnvironmentConstantImpl cut = new EnvironmentConstantImpl();
+            assertThat(cut.getEnvName(), is(equalTo("prod")));
         }
 
     }
