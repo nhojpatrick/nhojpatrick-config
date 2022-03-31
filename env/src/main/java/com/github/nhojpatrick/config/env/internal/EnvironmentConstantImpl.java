326358aa5399edf32d1eb232aed70b28ddf84bf4
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-31 14:42:35 +0100

    Break Out Module 'env'

--- env/src/main/java/com/github/nhojpatrick/config/env/internal/EnvironmentConstantImpl.java
+++ env/src/main/java/com/github/nhojpatrick/config/env/internal/EnvironmentConstantImpl.java
@@ -1,6 +1,6 @@
-package com.github.nhojpatrick.config.core.internal;
+package com.github.nhojpatrick.config.env.internal;
 
-import com.github.nhojpatrick.config.core.EnvironmentConstant;
+import com.github.nhojpatrick.config.env.EnvironmentConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- env/src/test/java/com/github/nhojpatrick/config/env/internal/tests/EnvironmentConstantImplTest.java
+++ env/src/test/java/com/github/nhojpatrick/config/env/internal/tests/EnvironmentConstantImplTest.java
@@ -1,6 +1,6 @@
-package com.github.nhojpatrick.config.core.internal.tests;
+package com.github.nhojpatrick.config.env.internal.tests;
 
-import com.github.nhojpatrick.config.core.internal.EnvironmentConstantImpl;
+import com.github.nhojpatrick.config.env.internal.EnvironmentConstantImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
