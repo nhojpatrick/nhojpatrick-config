72b21ba642e842468b6bd60d5d0afd2b93313afd
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-31 13:43:16 +0100

    EnvironmentConstant.getDefault()

--- core/src/main/java/com/github/nhojpatrick/config/core/EnvironmentConstant.java
+++ core/src/main/java/com/github/nhojpatrick/config/core/EnvironmentConstant.java
@@ -2,6 +2,10 @@ package com.github.nhojpatrick.config.core;
 
 public interface EnvironmentConstant {
 
+    static EnvironmentConstant getDefault() {
+        return new EnvironmentConstantImpl();
+    }
+
     String getEnvName();
 
 }
