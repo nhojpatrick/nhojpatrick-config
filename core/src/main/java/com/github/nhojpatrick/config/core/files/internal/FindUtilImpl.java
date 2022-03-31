4bf337ca58241abdcee2433b318a2cd30ae03f0e
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-03-31 10:24:56 +0100

    FindUtil findFirstFileAsFile

--- core/src/main/java/com/github/nhojpatrick/config/core/files/internal/FindUtilImpl.java
+++ core/src/main/java/com/github/nhojpatrick/config/core/files/internal/FindUtilImpl.java
@@ -5,7 +5,11 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -17,6 +21,49 @@ public class FindUtilImpl
         implements FindUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FindUtilImpl.class);
+
+    @SuppressFBWarnings(value = {"FII_USE_METHOD_REFERENCE", "SLF4J_UNKNOWN_ARRAY"},
+            justification = "ccepted will look at changing")
+    public Optional<File> findFirstFileAsFile(final String... filesAsVarArgs) {
+
+        LOGGER.debug("findFirstFileAsFile('{}')", filesAsVarArgs);
+
+        requireNonNull(filesAsVarArgs, "List of files required.");
+
+        final List<String> files = asList(filesAsVarArgs);
+
+        final Optional<Boolean> anyNull = files.stream()
+                .map(p -> Objects.nonNull(p))
+                .filter(p -> p)
+                .findAny();
+        anyNull
+                .orElseThrow(() -> new NullPointerException("List of files required."));
+
+        LOGGER.debug("findFirstFileAsFile('{}') files '{}'", filesAsVarArgs, files);
+
+        final Optional<File> file = files.stream()
+                .filter(Objects::nonNull)
+                .map(FindUtilImpl.class.getClassLoader()::getResource)
+                .filter(Objects::nonNull)
+                .map(p -> {
+                    try {
+                        return p.toURI();
+                    } catch (URISyntaxException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .map(Paths::get)
+                .filter(Objects::nonNull)
+                .map(Path::toFile)
+                .filter(Objects::nonNull)
+                .filter(File::isFile)
+                .findFirst();
+//                .orElseThrow(() -> new RuntimeException(String.format("No File found from '%s'", files)));
+
+        LOGGER.debug("findFirstFileAsFile('{}') files '{}' found '{}'", filesAsVarArgs, files, file);
+        return file;
+    }
 
     @SuppressFBWarnings(value = {"FII_USE_METHOD_REFERENCE", "SLF4J_UNKNOWN_ARRAY"},
             justification = "ccepted will look at changing")
---
+++ core/src/test/java/com/github/nhojpatrick/config/core/files/internal/tests/FindUtil_findFirstFileAsFileTest.java
@@ -0,0 +1,142 @@
+package com.github.nhojpatrick.config.core.files.internal.tests;
+
+import com.github.nhojpatrick.config.core.files.internal.FindUtilImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.github.nhojpatrick.hamcrest.optionals.IsOptional.optionalIsEmpty;
+import static com.github.nhojpatrick.hamcrest.optionals.IsOptional.optionalIsPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FindUtil_findFirstFileAsFileTest {
+
+    private FindUtilImpl cut;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.cut = new FindUtilImpl();
+    }
+
+    @Test
+    @DisplayName("Input null single")
+    public void input_null() {
+        final Executable testMethod = () -> this.cut.findFirstFileAsFile(
+                null
+        );
+        final RuntimeException thrown = assertThrows(RuntimeException.class, testMethod);
+        assertAll("Checking Exception",
+                () -> assertThat(thrown.getMessage(), is(equalTo("List of files required."))),
+                () -> assertThat(thrown.getCause(), is(nullValue()))
+        );
+    }
+
+    @Test
+    @DisplayName("Input null multiple")
+    public void input_null_multiples() {
+        final Executable testMethod = () -> this.cut.findFirstFileAsFile(
+                null,
+                null
+        );
+        final RuntimeException thrown = assertThrows(RuntimeException.class, testMethod);
+        assertAll("Checking Exception",
+                () -> assertThat(thrown.getMessage(), is(equalTo("List of files required."))),
+                () -> assertThat(thrown.getCause(), is(nullValue()))
+        );
+    }
+
+    @Test
+    @DisplayName("Input unknown single")
+    public void input_unknown() {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/unknown.file"
+        );
+        assertThat(actual, is(optionalIsEmpty()));
+    }
+
+    @Test
+    @DisplayName("Input unknown multiple")
+    public void input_unknown_multiple() {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/unknownA.file",
+                "com/github/nhojpatrick/config/core/files/tests/unknownB.file"
+        );
+        assertThat(actual, is(optionalIsEmpty()));
+    }
+
+    @Test
+    @DisplayName("Input valid single")
+    public void input_valid()
+            throws IOException {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/abc.file"
+        );
+        assertThat(actual, is(optionalIsPresent()));
+        final String actualAbsolutePath = actual.get().getAbsolutePath();
+        assertThat(actualAbsolutePath, is(endsWith("com/github/nhojpatrick/config/core/files/tests/abc.file")));
+    }
+
+    @Test
+    @DisplayName("Input valid abc def")
+    public void input_valid_abc_def()
+            throws IOException {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/abc.file",
+                "com/github/nhojpatrick/config/core/files/tests/def.file"
+        );
+        assertThat(actual, is(optionalIsPresent()));
+        final String actualAbsolutePath = actual.get().getAbsolutePath();
+        assertThat(actualAbsolutePath, is(endsWith("com/github/nhojpatrick/config/core/files/tests/abc.file")));
+    }
+
+    @Test
+    @DisplayName("Input valid def abc")
+    public void input_valid_def_abc()
+            throws IOException {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/def.file",
+                "com/github/nhojpatrick/config/core/files/tests/abc.file"
+        );
+        assertThat(actual, is(optionalIsPresent()));
+        final String actualAbsolutePath = actual.get().getAbsolutePath();
+        assertThat(actualAbsolutePath, is(endsWith("com/github/nhojpatrick/config/core/files/tests/def.file")));
+    }
+
+    @Test
+    @DisplayName("Input valid leading invalid")
+    public void input_valid_unknown_abc()
+            throws IOException {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/unknown.file",
+                "com/github/nhojpatrick/config/core/files/tests/abc.file"
+        );
+        assertThat(actual, is(optionalIsPresent()));
+        final String actualAbsolutePath = actual.get().getAbsolutePath();
+        assertThat(actualAbsolutePath, is(endsWith("com/github/nhojpatrick/config/core/files/tests/abc.file")));
+    }
+
+    @Test
+    @DisplayName("Input valid trailing invalid")
+    public void input_valid_abc_unknown()
+            throws IOException {
+        final Optional<File> actual = this.cut.findFirstFileAsFile(
+                "com/github/nhojpatrick/config/core/files/tests/abc.file",
+                "com/github/nhojpatrick/config/core/files/tests/unknown.file"
+        );
+        assertThat(actual, is(optionalIsPresent()));
+        final String actualAbsolutePath = actual.get().getAbsolutePath();
+        assertThat(actualAbsolutePath, is(endsWith("com/github/nhojpatrick/config/core/files/tests/abc.file")));
+    }
+
+}
