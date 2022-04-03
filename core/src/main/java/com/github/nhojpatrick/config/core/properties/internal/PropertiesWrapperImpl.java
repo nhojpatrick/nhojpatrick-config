e53220049377e854ae7b1ac0a6f78133ab145f12
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-03 15:49:55 +0100

    PropertiesWrapper loading config files

--- core/src/main/java/com/github/nhojpatrick/config/core/properties/internal/PropertiesWrapperImpl.java
+++ core/src/main/java/com/github/nhojpatrick/config/core/properties/internal/PropertiesWrapperImpl.java
@@ -2,10 +2,23 @@ package com.github.nhojpatrick.config.core.properties.internal;
 
 import com.github.nhojpatrick.config.core.properties.PropertiesWrapper;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.configuration2.CombinedConfiguration;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.builder.fluent.PropertiesBuilderParameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.tree.MergeCombiner;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,7 +32,11 @@ public class PropertiesWrapperImpl
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesWrapperImpl.class);
 
-    public PropertiesWrapperImpl(final String... filesAsVarArgs) {
+    private final Configuration config;
+
+    public PropertiesWrapperImpl(final String... filesAsVarArgs)
+            throws ConfigurationException,
+            URISyntaxException {
 
         LOGGER.debug("PropertiesWrapperImpl('{}')", new Object[]{filesAsVarArgs});
 
@@ -42,6 +59,36 @@ public class PropertiesWrapperImpl
         anyNull.ifPresent(p -> {
             throw new NullPointerException("List of files required.");
         });
+
+        final CombinedConfiguration config = new CombinedConfiguration(new MergeCombiner());
+
+        final Parameters params = new Parameters();
+
+        for (final String fileAsStr : files) {
+
+            final URL fileAsUrl = PropertiesWrapperImpl.class.getResource(fileAsStr);
+            if (fileAsUrl == null) {
+                continue;
+            }
+
+            final File file = Paths.get(fileAsUrl.toURI())
+                    .toFile();
+            if (!file.isFile()) {
+                continue;
+            }
+
+            final String fileAsAbsolutePath = file.getAbsolutePath();
+
+            final PropertiesBuilderParameters pbp = params.properties()
+                    .setFileName(fileAsAbsolutePath);
+            final FileBasedConfigurationBuilder<FileBasedConfiguration> fbcb
+                    = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                    .configure(pbp);
+
+            config.addConfiguration(fbcb.getConfiguration());
+        }
+
+        this.config = config;
     }
 
 }
