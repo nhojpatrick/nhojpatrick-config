ce346a38ab96947ba6adffcc2f0daf06160140be
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-03 16:51:38 +0100

    PropertiesWrapper trimToNull strings

--- core/src/main/java/com/github/nhojpatrick/config/core/properties/internal/PropertiesWrapperImpl.java
+++ core/src/main/java/com/github/nhojpatrick/config/core/properties/internal/PropertiesWrapperImpl.java
@@ -2,12 +2,14 @@ package com.github.nhojpatrick.config.core.properties.internal;
 
 import com.github.nhojpatrick.config.core.properties.PropertiesWrapper;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
@@ -23,7 +25,10 @@ public class PropertiesWrapperImpl
 
         requireNonNull(filesAsVarArgs, "List of files required.");
 
-        final List<String> files = asList(filesAsVarArgs);
+        final List<String> files = asList(filesAsVarArgs)
+                .stream()
+                .map(p -> StringUtils.trimToNull(p))
+                .collect(Collectors.toList());
 
         if (CollectionUtils.isEmpty(files)) {
             throw new NullPointerException("List of files required.");
