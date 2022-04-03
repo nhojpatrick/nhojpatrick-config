e199414584c798cbc275c5498fea814b62d74fac
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-03 14:08:46 +0100

    apache commons-collections4 usage

--- core/src/main/java11/module-info.java
+++ core/src/main/java11/module-info.java
@@ -2,6 +2,7 @@ module com.github.nhojpatrick.config.core {
     exports com.github.nhojpatrick.config.core;
     exports com.github.nhojpatrick.config.core.files;
     requires com.github.spotbugs.annotations;
+    requires org.apache.commons.collections4;
     requires org.apache.commons.lang3;
     requires org.slf4j;
 }
