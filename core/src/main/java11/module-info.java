ea8728331f327cb7695359dc66462467acf07a64
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-05 21:05:20 +0100

    Java11 module-info

--- core/src/main/java11/module-info.java
+++ core/src/main/java11/module-info.java
@@ -1,6 +1,10 @@
 module com.github.nhojpatrick.config.core {
     exports com.github.nhojpatrick.config.core;
+    exports com.github.nhojpatrick.config.core.internal
+            to com.github.nhojpatrick.config.core.test;
     exports com.github.nhojpatrick.config.core.files;
+    exports com.github.nhojpatrick.config.core.files.internal
+            to com.github.nhojpatrick.config.core.test;
     requires com.github.spotbugs.annotations;
     requires org.apache.commons.collections4;
     requires org.apache.commons.lang3;
---
+++ core/src/test/java11/module-info.java
@@ -0,0 +1,12 @@
+open module com.github.nhojpatrick.config.core.test {
+    requires com.github.nhojpatrick.config.core;
+    requires com.github.nhojpatrick.hamcrest.collections;
+    requires com.github.nhojpatrick.hamcrest.lang;
+    requires com.github.nhojpatrick.hamcrest.optionals;
+    requires org.apache.logging.log4j;
+    requires org.hamcrest;
+    requires org.junitpioneer;
+    requires transitive org.junit.jupiter.api;
+    requires transitive org.junit.jupiter.engine;
+    requires transitive org.junit.jupiter.params;
+}
